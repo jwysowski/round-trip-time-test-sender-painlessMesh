a7b8eb44a72ddd4392349a605e0724c9e8497797
Jakub Wysowski <kbwysowski@gmail.com>
2022-04-27 19:11:15 +0200

    Delete unused code

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ void received_callback(const uint32_t &from, const String &msg);
 painlessMesh mesh;
 String chip_id;
 void setup() {
-	Serial.begin(BAUDRATE);
+	// Serial.begin(BAUDRATE);
     chip_id = String(ESP.getChipId());
 
 	//mesh
